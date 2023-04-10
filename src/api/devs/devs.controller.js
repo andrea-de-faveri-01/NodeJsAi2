661f480a41e21b5d0eec7b84dc33fb1f6c70c436
andrea-de-faveri-01 <andreadefaveri01@gmail.com>
2023-04-10 02:08:08 +0100

    new devs  controller getDevById(dont'work)

--- src/api/devs/devs.controller.js
+++ src/api/devs/devs.controller.js
@@ -1,39 +1,47 @@
 const Dev = require("./devs.model");
 
 const postDev = async (req, res, next) => {
+  try {
+    const newDev = await new Dev(req.body);
 
-    try {
-
-        const newDev = await new Dev(req.body);
-
-        if (req.file) {
-            newDev.logo = req.file.path;
-          }
-      
-        await newDev.save();
-
-        return res.status(201).json(newDev);
-
-    } catch (error) {
-        return next(error)
+    if (req.file) {
+      newDev.logo = req.file.path;
     }
 
-}
+    await newDev.save();
 
-const getDevs = async (req, res, next) => {
+    return res.status(201).json(newDev);
+  } catch (error) {
+    return next(error);
+  }
+};
 
+const getAllDevs = async (req, res, next) => {
+  try {
+    const devs = await Dev.find().populate("ais");
+
+    return res.json(devs);
+  } catch (error) {
+    return next(error);
+  }
+};
+
+const getDevById = async (req, res, next) => {
     try {
-
-        
-        const devs = await Dev.find().populate("ais");
-
-
-        return res.json(devs);
-        
+      const { id } = req.params;
+      console.log('id:', id);
+      const dev = await Dev.findById(id).populate("ais");
+      console.log('dev:', dev);
+  
+      if (!dev) {
+        return res.status(404).json({ message: "Developer not found" });
+      }
+  
+      return res.json(dev);
     } catch (error) {
-        return next(error);
+      console.log('error:', error);
+      return next(error);
     }
+  };
 
-}
-
-module.exports = { postDev, getDevs };
+module.exports = { postDev, getAllDevs, getDevById };
--- src/api/devs/devs.routes.js
+++ src/api/devs/devs.routes.js
@@ -1,10 +1,11 @@
 const { isAuth, isAdmin } = require("../../middlewares/auth");
 const upload = require("../../middlewares/file");
-const { postDev, getDevs } = require("./devs.controller");
+const { postDev, getAllDevs, getDevById } = require("./devs.controller");
 
 const DevsRoutes = require("express").Router();
 
 DevsRoutes.post("/", upload.single("logo"), postDev);
-DevsRoutes.get("/", getDevs);
+DevsRoutes.get("/", getAllDevs);
+DevsRoutes.get("/:id", getDevById);
 
 module.exports = DevsRoutes;
